86321bb4ef85127ae663cdc5dbe158548178cba5
Marvin Zhang <tikazyq@163.com>
2024-08-17 19:29:35 +0800

    chore: updated comments

--- src/directives/click-outside/clickOutside.ts
+++ src/directives/click-outside/clickOutside.ts
@@ -3,9 +3,9 @@ import { Directive } from 'vue';
 const clickOutsideDirective: Directive = {
   beforeMount(el, binding) {
     el.clickOutsideEvent = function (event: MouseEvent) {
-      // 这里检查点击是否发生在元素外部
+      // Check that click was outside the element
       if (!(el == event.target || el.contains(event.target))) {
-        // 如果是外部点击，则调用传入的回调函数
+        // If yes, call method provided in attribute value
         binding.value(event);
       }
     };
